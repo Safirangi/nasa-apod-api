72d0584357d06f58ea11ee631ba85724e7660ddc
Swati A Firangi <swatifirangi29@gmail.com>
2022-06-24 22:18:37 +0530

    add code for img, title and explanation

--- nasa.js
+++ nasa.js
@@ -2,12 +2,18 @@ document.querySelector("button").addEventListener("click", getFetch);
 
 function getFetch() {
   const choice = document.querySelector("input").value.toLowerCase();
-  const url = `https://api.nasa.gov/planetary/apod?api_key=RSrYE8iBbjLq8Qnu4psZ13kHc7AVt1HBZdZJnJmM`;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=YAaedpPBm7wtIMOveorx9dMioDxj4jQRV7pqzggQ`;
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+    
+      document.querySelector("h3").innerText = data.title;
+
+      document.querySelector("p").innerText = data.explanation;
+
+      document.querySelector("img").src = data.hdurl;
     })
     .catch((err) => {
       console.log(`error ${err}`);
